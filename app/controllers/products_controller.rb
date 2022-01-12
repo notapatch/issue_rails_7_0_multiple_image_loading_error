1ef9c09f6515055fe0dba99da1c8683075307082
Richard <richard.wigley@gmail.com>
2022-01-12 10:25:14 +0000

    Product has_one_attached image and displays it in _product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   # GET /products or /products.json
   def index
-    @products = Product.all
+    @products = Product.with_attached_image.all
   end
 end
